32b67c91627f5b716062a4900384bab5f4401183
noulimitdmitry <kursant7321@gmail.com>
2024-01-11 11:12:56 +0200

    (createRoot)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/redux-store";
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
+const root = document.getElementById("root");
+
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  root
 );
+
 reportWebVitals();
